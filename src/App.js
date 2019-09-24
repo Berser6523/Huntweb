c4731cfe327e394853ce5237760944be55e3eb9f
Alisson <alisson_942@outlook.com>
2019-09-24 20:32:56 -0300

    config-router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import Header from './components/Header'
 import Main from './pages/main/index'
+import Route from './routes'
+
 
 import './styles/css/index.css'
 
@@ -9,7 +11,7 @@ export default () => {
   return (
     <div className="App"> 
       <Header />
-      <Main />
+      <Route />
     </div>
   );
 }
---
+++ src/pages/product/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Product extends Component {
+    state = {  }
+
+    render() { 
+        return ( <h1>Olá produto</h1> );
+    }
+}
+ 
+export default Product;
